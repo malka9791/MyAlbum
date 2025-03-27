702377797d6bc3a2f6f15c70ed49664b5d549f1d
malka9791 <m0527169791@gmail.com>
2025-03-27 20:58:01 +0200

    change program api

--- GalleryApi/GalleryApp/Program.cs
+++ GalleryApi/GalleryApp/Program.cs
@@ -94,11 +94,19 @@ builder.Services.AddAuthentication(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = string.Empty; // גורם לטעינת Swagger כברירת מחדל בכתובת הראשית
+});
+
 app.UseAuthentication();
 //end of jwt
 app.UseHttpsRedirection();
