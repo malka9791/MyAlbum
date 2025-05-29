d4de8a6d6999f63022e3737210ebb8d4097834da
malka9791 <m0527169791@gmail.com>
2025-05-29 22:33:32 +0300

    change add

--- GalleryAdmin/src/app/components/add-user/add-user.component.ts
+++ GalleryAdmin/src/app/components/add-user/add-user.component.ts
@@ -1,17 +1,30 @@
 import { Component } from '@angular/core';
 import { UserService } from '../../services/user/user.service';
 import { UserPost } from '../../models/userPost';
-import { MatFormField, MatLabel } from '@angular/material/form-field';
-import { MatIcon } from '@angular/material/icon';
+import { MatFormField, MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-user',
-  imports: [MatLabel, MatIcon, MatDialogModule, MatFormField],
+  imports: [MatLabel, MatIcon, MatDialogModule, MatFormField,CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSelectModule,
+    ],
   templateUrl: './add-user.component.html',
   styleUrl: './add-user.component.css',
 })
 export class AddUserComponent {
+role: any;
   constructor(private userService: UserService) {}
   ngOnInit(): void {}
   messege!: string;
@@ -22,6 +35,7 @@ export class AddUserComponent {
     this.userService.addUser(u1).subscribe({
       next: (response) => {
         this.messege = 'succes add a user!!';
+        window.location.reload();
       },
       error: (err) => {
         if (err.status === 403) {
